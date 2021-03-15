bf48261921aa09de47f6805df2cd30b84920ee06
Alireza <alireza.rayani@gmail.com>
2021-03-15 11:55:33 +0330

    add welcome post method

--- src/main/java/io/rayani/gitsimple/controller/UserController.java
+++ src/main/java/io/rayani/gitsimple/controller/UserController.java
@@ -1,6 +1,7 @@
 package io.rayani.gitsimple.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +11,9 @@ public class UserController {
     @GetMapping
     public String greeting() {
         return "Hello World";
+    }
+    @PostMapping
+    public String welcome(){
+        return "Bye BYe";
     }
 }
