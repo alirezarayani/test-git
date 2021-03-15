985e16c7c12b179efc427462396ae38c28c6c1ad
Alireza <alireza.rayani@gmail.com>
2021-03-15 11:45:01 +0330

    first commit

---
+++ src/main/java/io/rayani/gitsimple/GitSimpleApplication.java
@@ -0,0 +1,13 @@
+package io.rayani.gitsimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSimpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitSimpleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/rayani/gitsimple/GitSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.rayani.gitsimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitSimpleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
